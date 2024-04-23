70fc43475af6d95db29d22e643da180a0dd1a68c
motorbottle <motorbottt@gmail.com>
2024-04-23 18:47:49 -0400

    V0.1.1

--- install_dependencies.cmd
+++ install_dependencies.cmd
@@ -1,10 +1,10 @@
 @echo off
 SETLOCAL
 
-REM Set the directory where Python is located
-SET PYTHON_DIR=%~dp0python
+@REM REM Set the directory where Python is located
+@REM SET PYTHON_DIR=%~dp0python
 
-REM Add Python and the Scripts directory to the PATH
-SET PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts;%PYTHON_DIR%\Lib;%PATH%
+@REM REM Add Python and the Scripts directory to the PATH
+@REM SET PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts;%PYTHON_DIR%\Lib;%PATH%
 
 ENDLOCAL
